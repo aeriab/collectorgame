dd170568075cf3d7c9390ca7061cedee7b076351
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-10-03 23:03:52 -0700

    Balls despawn

--- Global.gd
+++ Global.gd
@@ -1,4 +1,4 @@
 extends Node
 
-var spawn_rate: float = 1.0
+var spawn_rate: float = 0.01
 var shrink_speed: float = 0.01
--- ball.gd
+++ ball.gd
@@ -1,1 +1,13 @@
 extends RigidBody2D
+
+@onready var cue_ball_temp = $BallProxy/CueBallTemp
+@onready var cpu_particles_2d = $CPUParticles2D
+
+func _on_timer_timeout():
+	cue_ball_temp.visible = false
+	cpu_particles_2d.emitting = true
+	#queue_free()
+
+
+func _on_cpu_particles_2d_finished():
+	queue_free()
